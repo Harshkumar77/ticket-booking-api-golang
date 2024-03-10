e8f408df15869725aa6d737e2b3db10038b8b3d1
volcano <volcano@example.com>
2024-03-10 19:09:41 +0530

    Added cache and spilited handlers

--- main.go
+++ main.go
@@ -1,38 +1,72 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"time"
 
+	"github.com/charmbracelet/log"
 	"github.com/dotenv-org/godotenvvault"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
+	"github.com/gofiber/fiber/v2/utils"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
 	err := godotenvvault.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
 	app := fiber.New()
+	api := app.Group("/api")
 
 	ConnectDB()
 	defer DB.Close()
 
-	if os.Getenv("GO_ENV") == "dev" {
-		app.Get("/test", func(c *fiber.Ctx) error {
-			var version string
-			err = DB.QueryRow("select version()").Scan(&version)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-				os.Exit(1)
-			}
+	// Cached requests
+	Cache := cache.New(cache.Config{
+		Expiration: time.Hour,
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return utils.CopyString(c.Path())
+		},
+		Methods: []string{fiber.MethodGet},
+	})
+	api.Use([]string{
+		"/test",
+	}, Cache)
 
-			return c.JSON(fiber.Map{
-				"message":         "Hello from server",
-				"time":            time.Now(),
-				"message_from_db": version,
-			})
-		})
-	}
-
+	api.Get("/test", TestHandler)
 
 	app.Listen(":" + os.Getenv("PORT"))
+
+}
+
+// Hash password
+
+func hashPassword(password string) (string, error) {
+
+	// Convert password string to byte slice
+
+	var passwordBytes = []byte(password)
+
+	// Hash password with Bcrypt's min cost
+
+	hashedPasswordBytes, err := bcrypt.
+		GenerateFromPassword(passwordBytes, bcrypt.MinCost)
+
+	return string(hashedPasswordBytes), err
+
+}
+
+// Check if two passwords match using Bcrypt's CompareHashAndPassword
+
+// which return nil on success and an error on failure.
+
+func doPasswordsMatch(hashedPassword, currPassword string) bool {
+
+	err := bcrypt.CompareHashAndPassword(
+		[]byte(hashedPassword), []byte(currPassword))
+
+	return err == nil
+
 }
